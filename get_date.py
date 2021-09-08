f9c4058d5c044c608cd9cbfd14b9958672bdb983
Memozir <gizmovap@gmail.com>
2021-09-08 18:44:20 +0300

    time_zone

--- get_date.py
+++ get_date.py
@@ -1,8 +1,11 @@
 from datetime import datetime
+import pytz
+
+timezone_moscow = pytz.timezone('Europe/Moscow')
 
 
 def get_date():
-    return datetime.now()
+    return datetime.gmtnow(timezone_moscow)
 
 
 if __name__ == "__main__":
